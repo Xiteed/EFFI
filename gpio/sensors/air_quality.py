6262292cf7295218f698ab7d64f27ec80957b64f
Niels Anderßon <niels_ocke.andersson@nordakademie.de>
2022-11-29 16:28:57 +0100

    integrate gas sensor into air_quality

--- gpio/sensors/air_quality.py
+++ gpio/sensors/air_quality.py
@@ -1,20 +1,47 @@
 import time
 from seeed_dht import DHT
+from grove.adc import ADC
 from db_handling import upload_data
 
 HUMI = 0
 TEMP = 0
+GAS = 0
+
+
+class GroveGasSensorMQ2:
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+
+    @property
+    def MQ2(self):
+        value = self.adc.read(self.channel)
+        return value
+
+
+Grove = GroveGasSensorMQ2
+
+
+def measure_gas():
+    sensor = GroveGasSensorMQ2(0)
+    gas = sensor.MQ2
+    if gas != GAS:
+        GAS = gas
+    return GAS
 
 
 def measure_temp_hum():  # Grove - Temperature&Humidity Sensor connected to port D5
-    global HUMI, TEMP
-    sensor = DHT('11', 5)
+    global HUMI, TEMP, GAS
+    temphum_sensor = DHT('11', 5)
+    gas_sensor = GroveGasSensorMQ2(0)
     while True:
-        humi, temp = sensor.read()
-        if humi != HUMI or temp != TEMP:
+        humi, temp = temphum_sensor.read()
+        gas = gas_sensor.MQ2
+        if humi != HUMI or temp != TEMP or gas != GAS:
             HUMI = humi
             TEMP = temp
-            payload = f"temp_test temperature={temp},humidity={humi}\n"
+            GAS = gas
+            payload = f"temp_test temperature={temp},humidity={humi},gas={gas}\n"
             upload_data(payload)
         time.sleep(1)
 
--- gpio/sensors/water.py
+++ gpio/sensors/water.py
@@ -1,26 +1,28 @@
 import time
 from grove.adc import ADC
- 
+
 __all__ = ["GroveMoistureSensor"]
- 
+
+
 class GroveMoistureSensor:
 
     def __init__(self, channel):
         self.channel = channel
         self.adc = ADC()
- 
+
     @property
     def moisture(self):
 
         value = self.adc.read_voltage(self.channel)
         return value
- 
+
+
 Grove = GroveMoistureSensor
- 
- 
+
+
 def main():
     sensor = GroveMoistureSensor(2)
- 
+
     print('Detecting moisture...')
     while True:
         m = sensor.moisture
@@ -32,6 +34,7 @@ def main():
             result = 'Wet'
         print('Moisture value: {0}, {1}'.format(m, result))
         time.sleep(1)
- 
+
+
 if __name__ == '__main__':
     main()
