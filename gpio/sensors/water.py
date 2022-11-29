d931dec3db0d8f6257d4cdd540c0d242d75acd5f
PirminBoensch <99911239+PirminBoensch@users.noreply.github.com>
2022-11-29 16:33:58 +0100

    #2 moisture

--- gpio/sensors/water.py
+++ gpio/sensors/water.py
@@ -1,40 +1,33 @@
 import time
 from grove.adc import ADC
-
+from db_handling import upload_data
+ 
 __all__ = ["GroveMoistureSensor"]
-
+HUMI = 0
 
 class GroveMoistureSensor:
 
     def __init__(self, channel):
         self.channel = channel
         self.adc = ADC()
-
+ 
     @property
     def moisture(self):
-
         value = self.adc.read_voltage(self.channel)
         return value
-
-
+ 
 Grove = GroveMoistureSensor
-
-
+ 
 def main():
+    global MOIS
     sensor = GroveMoistureSensor(2)
-
-    print('Detecting moisture...')
     while True:
-        m = sensor.moisture
-        if 0 <= m and m < 300:
-            result = 'Dry'
-        elif 300 <= m and m < 600:
-            result = 'Moist'
-        else:
-            result = 'Wet'
-        print('Moisture value: {0}, {1}'.format(m, result))
+        mois = sensor.moisture
+        if mois != MOIS:
+            MOIS = mois
+        payload = f"water_test moisture={mois}\n"
+        upload_data(payload)
         time.sleep(1)
-
-
+ 
 if __name__ == '__main__':
     main()
