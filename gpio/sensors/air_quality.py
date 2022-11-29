f4d2a6e54757b5de91f136290dd13d3a52ef74af
Niels Anderßon <niels_ocke.andersson@nordakademie.de>
2022-11-29 15:44:08 +0100

    integrate data upload of airquality data

--- gpio/sensors/air_quality.py
+++ gpio/sensors/air_quality.py
@@ -1,12 +1,21 @@
 import time
 from seeed_dht import DHT
+from db_handling import upload_data
+
+HUMI = 0
+TEMP = 0
 
 
 def measure_temp_hum():  # Grove - Temperature&Humidity Sensor connected to port D5
+    global HUMI, TEMP
     sensor = DHT('11', 5)
     while True:
         humi, temp = sensor.read()
-        print(f'temperature {temp}C, humidity {humi}%')
+        if humi != HUMI or temp != TEMP:
+            HUMI = humi
+            TEMP = temp
+            payload = f"temp_test temperature={temp},humidity={humi}\n"
+            upload_data(payload)
         time.sleep(1)
 
 
@@ -15,6 +24,3 @@ if __name__ == '__main__':
         measure_temp_hum()
     except KeyboardInterrupt:
         print("\nKeyboard interrupt, cleanup is done")
-    finally:
-        pwm.stop()
-        GPIO.cleanup()
--- gpio/sensors/db_handling.py
+++ gpio/sensors/db_handling.py
@@ -1,15 +1,11 @@
 import requests
 
 URL = 'https://corlysis.com:8086/write'
-PARAMS = {"db": "test_db", "u": "token",
+PARAMS = {"db": "effidb", "u": "token",
           "p": "1d612c67390c15daa4ab59dcf8016f2c"}
 
 
 def upload_data(payload):
     r = requests.post(URL, params=PARAMS, data=payload)
-    print(f'Response code: {r.status_code}')
-
-
-if __name__ == '__main__':
-    payload = "temp_test temperature=26.64,humidity=15.53\n"
-    upload_data(payload)
+    if (r.status_code != 204):
+        print(f"ERROR: {r.content}")
