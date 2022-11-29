a809998201eb519be5f437a48148c53a7a15ef68
Niels Anderßon <niels_ocke.andersson@nordakademie.de>
2022-11-29 17:55:46 +0100

    integrate tank volume

--- gpio/sensors/water.py
+++ gpio/sensors/water.py
@@ -1,33 +1,31 @@
 import time
+import math
 from grove.adc import ADC
 from db_handling import upload_data
- 
-__all__ = ["GroveMoistureSensor"]
-HUMI = 0
+from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
+from grove.grove_moisture_sensor import GroveMoistureSensor
 
-class GroveMoistureSensor:
 
-    def __init__(self, channel):
-        self.channel = channel
-        self.adc = ADC()
- 
-    @property
-    def moisture(self):
-        value = self.adc.read_voltage(self.channel)
-        return value
- 
-Grove = GroveMoistureSensor
- 
+MOIS = 0
+DIST = 0
+
+
 def main():
-    global MOIS
-    sensor = GroveMoistureSensor(2)
+    global MOIS, DIST
+    moisture_sensor = GroveMoistureSensor(2)
+    distance_sensor = GroveUltrasonicRanger(16)
     while True:
-        mois = sensor.moisture
-        if mois != MOIS:
+        dist = distance_sensor.get_distance()
+        mois = moisture_sensor.moisture
+        print(f"Distance: {dist}")
+        if mois != MOIS or dist != DIST:
             MOIS = mois
-        payload = f"water_test moisture={mois}\n"
-        upload_data(payload)
+            DIST = dist
+            volume = round((math.pi * 56.25 * (24 - dist)) / 1000, 2)
+            payload = f"water_test moisture={mois},tank_volume={volume}\n"
+            upload_data(payload)
         time.sleep(1)
- 
+
+
 if __name__ == '__main__':
     main()
