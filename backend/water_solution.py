bb660762c7e0bb6a02e40e920b6fd547ba0204df
PirminBoensch <99911239+PirminBoensch@users.noreply.github.com>
2022-12-17 12:40:39 +0100

    #3 app logic

--- backend/water_solution.py
+++ backend/water_solution.py
@@ -5,12 +5,13 @@ import pandas as pd
 from weather import get_precipiation
 
 # returns current tank level and moisture status 
+# we hard-coded moisture because moisture sensor is not working correctly
 def get_current_values():
     df = get_data('water_resources').iloc[-1:]
     tank_level = round(float(df['tank_volume'].values[0]), 2)
     return {'tank_level': tank_level, 'moisture': 'OK'}
 
-
+# returns predicted water tank level for in one week without water use 
 def get_predicted_water_level():
     data_json = {}
     with open('user_info.json', 'r') as file:
@@ -19,11 +20,11 @@ def get_predicted_water_level():
     current_value = get_current_values()['tank_level']
     predicted_value = current_value + get_precipiation() * float(data_json["roof_size"])
     if predicted_value > float(data_json["tank_volume"]):
-        return data_json["tank_volume"]
+        return float(data_json["tank_volume"])
     else:
         return round(predicted_value, 2)
 
-
+# detects if water is needed 
 def get_water_need():
     data_json = {}
     with open('water_config.json', 'r') as file:
@@ -32,18 +33,16 @@ def get_water_need():
     water_need = float(data_json[mydate.strftime("%B")]) - get_precipiation()
     return round(water_need, 2)
 
-
+# returns predicted water tank level for in one week with water use 
 def get_predicted_water_level_with_use():
     if is_watering_necessary():
         return get_predicted_water_level() - get_water_need()
     else:
         return get_predicted_water_level()
 
-
+# returns boolean if watering is neccesary
 def is_watering_necessary():
     if get_water_need() > 0:
         return True
     else:
         return False
-
-get_current_values()
