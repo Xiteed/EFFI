93bdd177340d74e1fccebe5dae7056369a05a80a
PirminBoensch <99911239+PirminBoensch@users.noreply.github.com>
2022-12-17 12:19:27 +0100

    #3 roof size

--- backend/water_solution.py
+++ backend/water_solution.py
@@ -4,11 +4,11 @@ from db_access import get_data
 import pandas as pd
 from weather import get_precipiation
 
-
+# returns current tank level and moisture status 
 def get_current_values():
-    # df = get_data('water_resources').iloc[-1:]
-    # return float(df['tank_volume'].values[0])
-    return {'tank_level': 2.05, 'moisture': 'OK'}
+    df = get_data('water_resources').iloc[-1:]
+    tank_level = round(float(df['tank_volume'].values[0]), 2)
+    return {'tank_level': tank_level, 'moisture': 'OK'}
 
 
 def get_predicted_water_level():
@@ -17,11 +17,11 @@ def get_predicted_water_level():
         data_json = json.loads(file.read())
 
     current_value = get_current_values()['tank_level']
-    predicted_value = current_value + get_precipiation()
+    predicted_value = current_value + get_precipiation() * float(data_json["roof_size"])
     if predicted_value > float(data_json["tank_volume"]):
         return data_json["tank_volume"]
     else:
-        return predicted_value
+        return round(predicted_value, 2)
 
 
 def get_water_need():
@@ -30,7 +30,7 @@ def get_water_need():
         data_json = json.loads(file.read())
     mydate = datetime.datetime.now()
     water_need = float(data_json[mydate.strftime("%B")]) - get_precipiation()
-    return water_need
+    return round(water_need, 2)
 
 
 def get_predicted_water_level_with_use():
@@ -45,3 +45,5 @@ def is_watering_necessary():
         return True
     else:
         return False
+
+get_current_values()
