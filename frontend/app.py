52cd1b6902db53a8b4aeef014eff2d2fa13c6b1b
Niels Anderßon <niels_ocke.andersson@nordakademie.de>
2022-12-20 18:22:52 +0100

    adjust app to start webapp

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,11 @@
-import socket
-# import subprocess
-# import datetime
+import subprocess
 from website import create_app
 
 app = create_app()
 
-h_name = socket.gethostname()
-IP_address = socket.gethostbyname(h_name)
-
-
 if __name__ == '__main__':
     # Open subprocess to get ip address of device.
-    # p = subprocess.Popen(['hostname -I'], shell=True, stdout=subprocess.PIPE)
-    # p.wait()
-    # IP_address = p.stdout.readline()[:-2].decode('UTF-8')
+    p = subprocess.Popen(['hostname -I'], shell=True, stdout=subprocess.PIPE)
+    p.wait()
+    IP_address = p.stdout.readline()[:-2].decode('UTF-8')
     app.run(debug=True, host=IP_address, port=8080)
